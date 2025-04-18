c3da800e6027f7f3583675389722a4e1f35bd747
Shivendra Panicker <panicks@amazon.com>
2025-04-18 14:40:53 -0700

    Address comments

--- pkg/providers/v1/delete_tags_batch.go
+++ pkg/providers/v1/delete_tags_batch.go
@@ -14,12 +14,13 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package batcher
+package aws
 
 import (
 	"context"
 	"fmt"
 	"github.com/mitchellh/hashstructure/v2"
+	batcher2 "k8s.io/cloud-provider-aws/pkg/providers/v1/batcher"
 	"k8s.io/cloud-provider-aws/pkg/providers/v1/iface"
 	"k8s.io/klog/v2"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -31,24 +32,24 @@ import (
 
 // DeleteTagsBatcher contains the batcher details
 type DeleteTagsBatcher struct {
-	batcher *Batcher[ec2.DeleteTagsInput, ec2.DeleteTagsOutput]
+	batcher *batcher2.Batcher[ec2.DeleteTagsInput, ec2.DeleteTagsOutput]
 }
 
 // NewDeleteTagsBatcher creates a NewDeleteTagsBatcher object
 func NewDeleteTagsBatcher(ctx context.Context, ec2api iface.EC2) *DeleteTagsBatcher {
-	options := Options[ec2.DeleteTagsInput, ec2.DeleteTagsOutput]{
+	options := batcher2.Options[ec2.DeleteTagsInput, ec2.DeleteTagsOutput]{
 		Name:          "delete_tags",
 		IdleTimeout:   100 * time.Millisecond,
 		MaxTimeout:    1 * time.Second,
 		MaxItems:      50,
-		RequestHasher: DeleteTagsHasher,
+		RequestHasher: deleteTagsHasher,
 		BatchExecutor: execDeleteTagsBatch(ec2api),
 	}
-	return &DeleteTagsBatcher{batcher: NewBatcher(ctx, options)}
+	return &DeleteTagsBatcher{batcher: batcher2.NewBatcher(ctx, options)}
 }
 
 // DeleteTags adds delete tag input to batcher
-func (b *DeleteTagsBatcher) DeleteTags(ctx context.Context, DeleteTagsInput *ec2.DeleteTagsInput) (*ec2.DeleteTagsOutput, error) {
+func (b *DeleteTagsBatcher) deleteTags(ctx context.Context, DeleteTagsInput *ec2.DeleteTagsInput) (*ec2.DeleteTagsOutput, error) {
 	if len(DeleteTagsInput.Resources) != 1 {
 		return nil, fmt.Errorf("expected to receive a single instance only, found %d", len(DeleteTagsInput.Resources))
 	}
@@ -58,7 +59,7 @@ func (b *DeleteTagsBatcher) DeleteTags(ctx context.Context, DeleteTagsInput *ec2
 
 // DeleteTagsHasher generates hash for different delete tag inputs
 // Same set of tags have same hash, so they get executed together
-func DeleteTagsHasher(ctx context.Context, input *ec2.DeleteTagsInput) uint64 {
+func deleteTagsHasher(ctx context.Context, input *ec2.DeleteTagsInput) uint64 {
 	hash, err := hashstructure.Hash(input.Tags, hashstructure.FormatV2, &hashstructure.HashOptions{SlicesAsSets: true})
 	if err != nil {
 		log.FromContext(ctx).Error(err, "failed hashing input tags")
@@ -66,9 +67,9 @@ func DeleteTagsHasher(ctx context.Context, input *ec2.DeleteTagsInput) uint64 {
 	return hash
 }
 
-func execDeleteTagsBatch(ec2api iface.EC2) BatchExecutor[ec2.DeleteTagsInput, ec2.DeleteTagsOutput] {
-	return func(ctx context.Context, inputs []*ec2.DeleteTagsInput) []Result[ec2.DeleteTagsOutput] {
-		results := make([]Result[ec2.DeleteTagsOutput], len(inputs))
+func execDeleteTagsBatch(ec2api iface.EC2) batcher2.BatchExecutor[ec2.DeleteTagsInput, ec2.DeleteTagsOutput] {
+	return func(ctx context.Context, inputs []*ec2.DeleteTagsInput) []batcher2.Result[ec2.DeleteTagsOutput] {
+		results := make([]batcher2.Result[ec2.DeleteTagsOutput], len(inputs))
 		firstInput := inputs[0]
 		// aggregate instanceIDs into 1 input
 		for _, input := range inputs[1:] {
@@ -89,14 +90,14 @@ func execDeleteTagsBatch(ec2api iface.EC2) BatchExecutor[ec2.DeleteTagsInput, ec
 				go func(input *ec2.DeleteTagsInput) {
 					defer wg.Done()
 					out, err := ec2api.DeleteTags(input)
-					results[idx] = Result[ec2.DeleteTagsOutput]{Output: out, Err: err}
+					results[idx] = batcher2.Result[ec2.DeleteTagsOutput]{Output: out, Err: err}
 
 				}(input)
 			}
 			wg.Wait()
 		} else {
 			for idx := range inputs {
-				results[idx] = Result[ec2.DeleteTagsOutput]{Output: output}
+				results[idx] = batcher2.Result[ec2.DeleteTagsOutput]{Output: output}
 			}
 		}
 		return results
